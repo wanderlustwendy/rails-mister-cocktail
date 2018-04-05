0e165e16ff502a962662fd0a4ce9c7f4af6740ce
Wendy Lee <wanderlustwendy@icloud.com>
2018-04-05 12:21:41 +0800

    passed all validation excep cocktail/ingredient unique pair

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -2,5 +2,5 @@ class Dose < ApplicationRecord
   validates :description, presence: true, uniqueness: true
   belongs_to :cocktail
   belongs_to :ingredient
-  # validates :ingredient, uniqueness: { scope: :cocktail }
+  validates_uniqueness_of :cocktail_id, :scope => :ingredient_id
 end
